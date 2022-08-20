01249cf0a1d5dbd99c4a3948c93abbc87ba33e7c
CoolandonRS <49355953+CoolandonRS@users.noreply.github.com>
2022-08-19 19:33:24 -0600

    Initial commit

---
+++ src/main/java/net/numra/scripter/Scripter.java
@@ -0,0 +1,10 @@
+package net.numra.scripter;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Scripter implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    
+    }
+}
